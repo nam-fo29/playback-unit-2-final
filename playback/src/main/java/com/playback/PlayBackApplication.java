4f45cc1e9f084a4eadd7f97b7cbdc503fb03cef8
Choffman <cmhoffman94@gmail.com>
2025-10-27 15:37:35 -0500

    Spring app set up

---
+++ playback/src/main/java/com/playback/PlayBackApplication.java
@@ -0,0 +1,13 @@
+package com.playback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayBackApplication.class, args);
+	}
+
+}
---
+++ playback/src/test/java/com/playback/PlayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
