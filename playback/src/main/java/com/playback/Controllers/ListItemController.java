38af3a01bca8bcfbdb6a534ee6492424119371e1
Choffman <cmhoffman94@gmail.com>
2025-11-03 16:33:42 -0600

    PostMapping added to ListItemController for updating a title on a list

--- playback/src/main/java/com/playback/Controllers/ListItemController.java
+++ playback/src/main/java/com/playback/Controllers/ListItemController.java
@@ -4,10 +4,7 @@ package com.playback.Controllers;
 import com.playback.Models.ListItemModel;
 import com.playback.Repositories.ListItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,4 +24,17 @@ public class ListItemController {
     public List<ListItemModel> getAllListItems() {
         return listItemRepository.findAll();
     }
+
+    @PostMapping("/{id}")
+    public ListItemModel updateListItem(@PathVariable("id") Long id, @RequestBody ListItemModel itemDetails) {
+        ListItemModel listItem = listItemRepository.findById(id).orElse(null);
+        if (listItem != null) {
+            listItem.setList(itemDetails.getList());
+            listItem.setMedia(itemDetails.getMedia());
+            return listItemRepository.save(listItem);
+        }
+        return null;
+    }
+
+
 }
