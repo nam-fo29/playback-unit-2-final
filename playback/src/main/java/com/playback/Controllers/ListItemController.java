a304b480ea52eaac711cb907a038516e6ad3538b
Choffman <cmhoffman94@gmail.com>
2025-11-03 16:29:13 -0600

    GetMapping added to ListItemController

--- playback/src/main/java/com/playback/Controllers/ListItemController.java
+++ playback/src/main/java/com/playback/Controllers/ListItemController.java
@@ -1,4 +1,30 @@
 package com.playback.Controllers;
 
+
+import com.playback.Models.ListItemModel;
+import com.playback.Repositories.ListItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "*")
 public class ListItemController {
+
+    @Autowired
+    private final ListItemRepository listItemRepository;
+
+    public ListItemController(ListItemRepository listItemRepository) {
+        this.listItemRepository = listItemRepository;
+    }
+
+    @GetMapping
+    public List<ListItemModel> getAllListItems() {
+        return listItemRepository.findAll();
+    }
 }
