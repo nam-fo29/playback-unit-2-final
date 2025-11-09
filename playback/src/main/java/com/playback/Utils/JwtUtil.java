5d25ca9b31c6737cb1e7cb992d32fb432f90f0a8
Choffman <cmhoffman94@gmail.com>
2025-11-08 22:12:15 -0600

    First pass at JWT utils

--- playback/src/main/java/com/playback/Utils/JwtUtil.java
+++ playback/src/main/java/com/playback/Utils/JwtUtil.java
@@ -1,4 +1,52 @@
 package com.playback.Utils;
 
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
 public class JwtUtil {
+
+    private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60 * 1000;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String jwtToken) {
+        return getClaimFromToken(jwtToken, Claims::getSubject);
+    }
+
+    private <T> T getClaimFromToken(String token, Function<Claims, T> claimResolver) {
+        final Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody();
+        return claimResolver.apply(claims);
+    }
+
+    public boolean validateToken(String jwtToken, UserDetails userDetails) {
+        final String username = getUsernameFromToken(jwtToken);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken));
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        final Date expiration = getClaimFromToken(jwtToken, Claims::getExpiration);
+        return expiration.before(new Date());
+    }
 }
