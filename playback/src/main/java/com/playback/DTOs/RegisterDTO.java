b7062b3ce11fafc0d9590b4b3fb5f3aee8a870cd
Choffman <cmhoffman94@gmail.com>
2025-11-08 21:36:57 -0600

    Added fixes to registration DTO

--- playback/src/main/java/com/playback/DTOs/RegisterDTO.java
+++ playback/src/main/java/com/playback/DTOs/RegisterDTO.java
@@ -4,6 +4,14 @@ public class RegisterDTO {
     private String username;
     private String password;
 
+    public RegisterDTO() {
+    }
+
+    public RegisterDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
