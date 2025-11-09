e269c2e9894f15b090b6750d61691e1ca293feda
Choffman <cmhoffman94@gmail.com>
2025-11-08 21:39:37 -0600

    Login DTO setup first pass

--- playback/src/main/java/com/playback/DTOs/LoginDTO.java
+++ playback/src/main/java/com/playback/DTOs/LoginDTO.java
@@ -1,4 +1,23 @@
 package com.playback.DTOs;
 
 public class LoginDTO {
+    private String username;
+    private String password;
+
+    public LoginDTO() {
+    }
+
+    public LoginDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
